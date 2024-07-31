f0a9b135f1462c86a4192f8bdf3da55ca12d39db
shreya <its.shreyish@gmail.com>
2024-07-31 16:22:47 +0530

    Updates

---
+++ precache-manifest.54e373c2b430903ac8777b76705db914.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241ab7ae5929322994ab383ab58fe065",
+    "url": "/your-repo-name/index.html"
+  },
+  {
+    "revision": "3cefdcf74f6f6ecd786b",
+    "url": "/your-repo-name/static/css/main.1f40723d.chunk.css"
+  },
+  {
+    "revision": "033295c4e3377fad6ad3",
+    "url": "/your-repo-name/static/js/2.5f7e188d.chunk.js"
+  },
+  {
+    "revision": "3cefdcf74f6f6ecd786b",
+    "url": "/your-repo-name/static/js/main.38fb7f3a.chunk.js"
+  },
+  {
+    "revision": "77fc2c9844dfb9b40a72",
+    "url": "/your-repo-name/static/js/runtime~main.deb37ce2.js"
+  }
+]);
